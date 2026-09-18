400f6cab62720930fc5b01be3355deb3e6c0954b
Matheus.moura <mateus.leitao6@aluno.ce.gov.br>
2026-09-18 11:53:59 -0300

    Implement user status checks and payment validation
    Added logic to check age, access level, and payment status.

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
+const nome = "Matheus Moura"
+const idade = 18
+const categoria = "comum"
+const possuiInscricao = true
+const suspenso = false
+const valorInscricao = 50
+const valorPago = 50
+const idadeStatus = "Idade permitida"
+const nivelAcesso = "organizador"
 
+
+if( idade >= 18 ){
+    console.log("Idade permitida")
+}else{
+    console.log("Idade não permitida")
+}
+
+if( nivelAcesso === "organizador" || "arbitro" ){
+    console.log("Acesso administrativo liberado")
+}else{
+    console.log("Acesso comum")
+}
+
+if ( idade >= 18  && possuiInscricao === true && suspenso != false ){
+    console.log("Credenciamento liberado")
+}else{
+    console.log("Credenciamento negado")
+}
+
+if( valorPago >= valorInscricao ){
+    console.log("Pagamento aprovado")
+}else{
+    console.log("Pagamento insuficiente")
+}
+
+let troco = valorPago - valorInscricao
+
+if( valorPago >= valorInscricao ){
+    troco = "tem troco"
+}else{
+    troco = 0
+}
+
+if( Credenciamentoliberado && Pagamentoaprovado ){
+    console.log("Participação confirmada")
+}else{
+    console.log("Participação não confirmada")
+}
+
+const resumo = ""
+
+module.exports = {
+    nome,
+    idade,
+    categoria,
+    possuiInscricao,
+    suspenso,
+    valorInscricao,
+    valorPago,
+    idadeStatus,
+    nivelAcesso,
+    acessoStatus,
+    pagamentoStatus,
+    troco,
+    statusCompeticao,
+    resumo
+}
